5296a912ef17289e22f3efeda52994a69904fb62
bwaterman8 <bwaterman8@gmail.com>
2021-07-12 15:21:33 -0400

    hello and start to variables

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Brian");
+
+        String myFavNum = "My favorite number is ";
+        int num = 8;
+
+        System.out.println(myFavNum + num);
+    }
+}
